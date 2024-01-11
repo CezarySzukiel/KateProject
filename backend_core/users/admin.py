f2bf25e7ebdb36fc5ba1f3384f332d3eb9f0f883
cezary <cezary.szukiel@gmail.com>
2024-01-11 12:25:35 +0100

    models added to admin

--- backend_core/users/admin.py
+++ backend_core/users/admin.py
@@ -3,10 +3,15 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from . import models
-# Register your models here.
+
 
 class CustomUserAdmin(UserAdmin):
     model = get_user_model()
 
 
+class UserSettingsAdmin(admin.ModelAdmin):
+    model = models.UserSettings
+
+
 admin.site.register(models.CustomUser, CustomUserAdmin)
+admin.site.register(models.UserSettings, UserSettingsAdmin)
