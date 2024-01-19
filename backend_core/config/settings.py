8ebfadbb6b75c3676228cce86500ef09c80d05bd
cezary <cezary.szukiel@gmail.com>
2024-01-20 00:22:28 +0100

    slugd added to models exercise, section, subsection (migrations)

--- backend_core/config/settings.py
+++ backend_core/config/settings.py
@@ -157,9 +157,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.CustomUser'
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
