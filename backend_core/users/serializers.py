6c77331fdbe038da059d6d5e1b0fe3aa0093ad12
Cezary <cezary.szukiel@gmail.com>
2024-04-08 22:17:00 +0200

    UserSettingsSerializer

---
+++ backend_core/users/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import UserSettings
+
+class UserSettingsSerializer(serializers.ModelSerializer):
+	# email = serializers.EmailField(source='user.email', read_only=True) etc
+
+	class Meta:
+		model = UserSettings
+		fields = ("user", "level", "points", "exercises", )
