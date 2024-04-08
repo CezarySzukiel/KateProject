f80edece0896100e6c89412a7f4017f3ae9966aa
Cezary <cezary.szukiel@gmail.com>
2024-04-08 22:27:40 +0200

    Users List View and User Detail View

--- backend_core/users/views.py
+++ backend_core/users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView,  RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+from .models import UserSettings
+from .serializers import UserSettingsSerializer
+
+
+class UsersList(ListCreateAPIView):
+	permission_classes = [AllowAny] #todo change it!
+	queryset = UserSettings.objects.all()
+	serializer_class = UserSettingsSerializer
+
+class UserDetail(RetrieveUpdateDestroyAPIView):
+	permission_classes = [AllowAny] #todo change it!
+	queryset = UserSettings.objects.all()
+	serializer_class UserSettingsSerializer
