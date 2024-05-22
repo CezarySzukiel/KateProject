f93f57c7ce331e92d160b89ba71cf0b1a85d6978
Cezary <cezary.szukiel@gmail.com>
2024-05-22 16:56:09 +0200

    exercises types described

---
+++ frontend/src/exerciseTypes.jsx
@@ -0,0 +1,42 @@
+const exerciseTypes = [
+{
+	type: 1,
+	description: "Jedna odpowiedź spośród A-D."
+},
+{
+	type: 2,
+	description: "Jedna odpowiedź spośród A-C oraz jedno uzasadnienie tej odpowiedzi spośród 1.-3. (ale tabelka może mieć różne rozmiary, np 2x2, 2x3)."
+},
+{
+	type: 3,
+	description: "Dwie odpowiedzi spośród A-F."
+},
+{
+	type: 4,
+	description: "Dwie odpowiedzi spośród A-G."
+},
+{
+	type: 5,
+	description: "Jedna odpowiedź spośród A-C oraz jedna odpowiedź spośród D-F."
+},
+{
+	type: 6,
+	description: "Jedna odpowiedź spośród A-D oraz jedna odpowiedź spośród E-H."
+},
+{
+	type: 7,
+	description: "Ocena prawdziwości zdań (lub dokończenie zdań)."
+},
+{
+	type: 8,
+	description: "Dobieranie/przyporządkowanie/zestawienie odpowiedzi (spośród podanych) do określonych sytuacji/obiektów/elementów."
+},
+{
+	type: 9,
+	description: "Zadanie otwarte(1-4 punkty)"
+},
+{
+	type: 10,
+	description: "Wiązka zadań (zestaw od dwóch do czterech zadań występujących we wspólnym kontekście tematycznym, przy czym każde zadanie można rozwiązać niezależnie od rozwiazania innych zadań w danej wiązce. Wiązka zadań może się składać zarówno z zadań zamkniętych, jak i z zadań otwartych)."
+},
+]
