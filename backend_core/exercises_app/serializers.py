564f5012a1c489a250c1d171ac733802304d72d8
Cezary <cezary.szukiel@gmail.com>
2024-06-14 15:24:02 +0200

    id field added to Function serializer

--- backend_core/exercises_app/serializers.py
+++ backend_core/exercises_app/serializers.py
@@ -23,7 +23,8 @@ class FunctionSerializer(serializers.ModelSerializer):
     """Serializer for Function model."""
     class Meta:
         model = Function
-        fields = ('function_type', 'a', 'b', 'c', 'coefficients', 'x_start', 'x_end', 'x_step', 'x_offset', 'y_offset')
+        fields = ('id','function_type', 'a', 'b', 'c', 'coefficients', 'x_start', 'x_end', 'x_step', 'x_offset', 'y_offset')
+        read_only_fields = ('id',)
 
 
 class ExerciseDetailSerializer(serializers.ModelSerializer):
