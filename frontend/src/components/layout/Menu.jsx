fe00813d813a0d24c71995ca7a219ed9fda93cf0
cezary <cezary.szukiel@gmail.com>
2024-09-04 10:26:29 +0200

    Sidebar position fixed (stays in place when scrolling)

--- frontend/src/components/layout/Menu.jsx
+++ frontend/src/components/layout/Menu.jsx
@@ -1,42 +1,50 @@
 import "./menu.css"
-import React, { useState } from 'react';
-import { Link } from "react-router-dom";
-import { useNavigate } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link} from "react-router-dom";
+import {useNavigate} from 'react-router-dom';
 
 
 export function Menu(props) {
-	const [isMenuOpen, setIsMenuOpen] = useState(false);
-	const navigate = useNavigate();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const navigate = useNavigate();
 
     const handleMenuHover = (isHovering) => {
         setIsMenuOpen(isHovering);
     }
 
     return (
-    	<div className="dropdown"
-                onMouseEnter={() => handleMenuHover(true)}
-                onMouseLeave={() => handleMenuHover(false)}
-            >
-                <button className="dropbtn">Menu</button>
-                <div className="dropdownOptions">
-                    {isMenuOpen && (
-                        <>
-                            {props.isLoggedIn && 
+        <div className="dropdown"
+             onMouseEnter={() => handleMenuHover(true)}
+             onMouseLeave={() => handleMenuHover(false)}
+        >
+            <button className="dropbtn">Menu</button>
+            <div className="dropdownOptions">
+                {isMenuOpen && (
+                    <>
+                        {props.isLoggedIn &&
                             <>
-                            	<Link to={`user`}><button>użytkownik</button></Link>
-                            	<Link to={`logout`}><button>Wyloguj</button></Link>
-                        	</>
-                        	}
-                        	{!props.isLoggedIn &&
-                        	<>
-                            <Link to={`login`}><button>Zaloguj</button></Link>
-                            <Link to={`register`}><button>Zarejestruj</button></Link>
+                                <Link to={`user`}>
+                                    <button>użytkownik</button>
+                                </Link>
+                                <Link to={`logout`}>
+                                    <button>Wyloguj</button>
+                                </Link>
                             </>
-                        	}
-                        </>
-                    )}
-                </div>
+                        }
+                        {!props.isLoggedIn &&
+                            <>
+                                <Link to={`login`}>
+                                    <button>Zaloguj</button>
+                                </Link>
+                                <Link to={`register`}>
+                                    <button>Zarejestruj</button>
+                                </Link>
+                            </>
+                        }
+                    </>
+                )}
             </div>
+        </div>
     )
 
 }
